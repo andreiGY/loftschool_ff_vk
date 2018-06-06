0a21764ae1621b15ec075b2dcd3ea207233b01fa
Gladarenko Andrei <a.gladarenko@gmail.com>
2018-06-06 12:58:45 +0300

    требуется дорабокта фильтрации и dragndrop

--- filter.js
+++ filter.js
@@ -3,54 +3,67 @@ var filter_list = new Array(); //фильтр друзей
 var friends_list = new Array(); // все друзья
 
 /* читает localstorage для извлечения фильтра друзей */
-function readStorage() {
-    let filtered = localStorage.getItem("filtered_friends");
-    console.log(JSON.parse(filtered));
-    return JSON.parse(filtered);
+/*
+filtered_friends -  фильтр друзей
+allfriends - все друзья
+*/
+function readStorage(storageName) {
+    let storage = localStorage.getItem(storageName);
+    console.log(JSON.parse(storage));
+    return JSON.parse(storage);
 }
 
 /* обновляет localstorage измененным фильтром */
-function updateStorage(filtered_friends) {
-    localStorage.setItem("filtered_friends", JSON.stringify(filtered_friends));
-    console.log(JSON.stringify(filtered_friends));
-    return;
+function updateStorage(data, storageName) {
+    localStorage.setItem(storageName, JSON.stringify(data));
+    console.log(JSON.stringify(data));
 }
 
+/* добавляет друга в фильтр */
 function add2filter(event, id) {   
     console.log("id is: " + id);
     for(let fr in friends_list) {
         if(friends_list[fr].id == id) {
             console.log(friends_list[fr]);
-            filter_list.push(friends_list[fr]);
-            updateStorage(filter_list);
-           friends_list.splice(fr, 1);
-            showFilter();
-            refreshFriends();
+            filter_list == null? filter_list[0] = friends_list[fr] : filter_list.push(friends_list[fr]);          
+            updateStorage(filter_list, "filtered_friends");
+            friends_list.splice(fr, 1);
+            updateStorage(friends_list, "friends_list");
+            refreshListView(friends_list, "friend-template","allfriends");
+            refreshListView(filter_list, "filter-template","filtered"); 
         }
     }
 }
 
-
+/* удаляет друга из фильтра */
 function removeFromFilter(event, id) {
     console.log("id is: " + id);
     for(let fi in filter_list) {
         if(filter_list[fi].id == id) {
-            filter_list.splice(fi, 1); 
-            updateStorage(filter_list);
             //  http://www.codexpedia.com/javascript/javascript-loop-through-array-and-object-properties/              
             friends_list.push(filter_list[fi]);
-            refreshFriends();
-            showFilter();
+            updateStorage(friends_list, "friends_list");
+            filter_list.splice(fi, 1); 
+            updateStorage(filter_list, "filtered_friends");
+            refreshListView(friends_list, "friend-template","allfriends");
+            refreshListView(filter_list, "filter-template","filtered"); 
 
         }
     }
 }
 
 function searchInPanel(searchStr, list) {
-    if(list == "friends") {
-        
-    } else {
+    if(searchStr == "") return;
 
-    }
+        for(let it in list) {
+            if(list[it].first_name.search(searchStr) != -1 || list[it].last_name.search(searchStr) != -1 ) {
+                list.splice(it,1);
+            }
+        }
+        console.log( "is filter list?" + list === filter_list);
+        // https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+        
+        console.log("is friends list?  " + list == friends_list);
+         list === filter_list? refreshListView(filter_list, "filter-template","filtered"): refreshListView(friends_list, "friend-template","allfriends");
 }
 
--- main.js
+++ main.js
@@ -43,41 +43,40 @@ function callAPI(method, params) {
 
         const rawfriends = await callAPI('friends.get', { fields: 'nickname, photo_50, first_name, last_name', count: 10 });
         const friends = rawfriends.items;
-        const template = document.querySelector('#friend-template').textContent;
-        const render = Handlebars.compile(template);
-        const html = render(friends);
-        const allfriends = document.querySelector('#allfriends');
+       // const template = document.querySelector('#friend-template').textContent;
+       // const render = Handlebars.compile(template);
+      //  const html = render(friends);
+     //   const allfriends = document.querySelector('#allfriends');
 
-        allfriends.innerHTML = html;
-        console.log(JSON.stringify(friends));
+      //  allfriends.innerHTML = html;
+      //  console.log(JSON.stringify(friends));
+        //сохранить результат в localstorage:
         friends_list = friends;
+        updateStorage(friends_list, "allfriends");
+        refreshListView(friends_list, "friend-template","allfriends");
+
     } catch (e) {
         console.error(e);
     }
 })();
 
-function showFilter() {
-    let stored = readStorage();
-    const ftemplate = document.querySelector('#filter-template').textContent;
-    const frender = Handlebars.compile(ftemplate);
-    const fhtml = frender(stored);
-    const filtered = document.querySelector('#filtered');
-
-    filtered.innerHTML = fhtml;
-    console.log(JSON.stringify(filtered));
-    filter = filtered;
-
+function showFilter(fromStorage) {
+    if(fromStorage) {
+        let result = readStorage("filter_list");
+        if(result !== null) filter_list = result;    
+    }
+    console.log("filter_list contents - " + JSON.stringify(filter_list));
+    refreshListView(filter_list, "filter-template","filtered"); 
 }
 
-showFilter();
 
-function refreshFriends() {
-    const rtemplate = document.querySelector('#friend-template').textContent;
+function refreshListView(listName, templateName, parentNode) {   
+    const rtemplate = document.querySelector("#" + templateName).textContent;
     const rrender = Handlebars.compile(rtemplate);
-    const rhtml = rrender(friends_list);
-    const rfriends = document.querySelector('#allfriends');
-
+    const rhtml = rrender(listName);
+    const rfriends = document.querySelector("#" + parentNode);
     rfriends.innerHTML = rhtml;
     console.log(JSON.stringify(rfriends));
-   // filter = filtered;
 }
+
+showFilter(true);
